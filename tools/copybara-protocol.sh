6adef7c31be99683572faf7d26e8413f250f528a
Sam Gammon <sam.g@cookies.co>
2020-09-07 22:00:58 -0700

    Ignore noops from Copybara

--- tools/copybara-protocol.sh
+++ tools/copybara-protocol.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Performing migration for external repo 'protocol'..."
-java -jar ~/bin/copybara.jar migrate copy.bara.sky protocol;
+java -jar ~/bin/copybara.jar migrate copy.bara.sky protocol --ignore-noop;
 
--- tools/copybara-python.sh
+++ tools/copybara-python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Performing migration for external repo 'python'..."
-java -jar ~/bin/copybara.jar migrate copy.bara.sky python;
+java -jar ~/bin/copybara.jar migrate copy.bara.sky python --ignore-noop;
 
