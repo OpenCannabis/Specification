ea4cf01c03d2380fedc338f0f49bce85f9c908fa
Sam Gammon <sam.g@cookiescalifornia.com>
2021-10-20 14:53:17 -0700

    Disable GH java artifact publish

--- sdk/java/publish_github.sh
+++ sdk/java/publish_github.sh
@@ -3,13 +3,13 @@
 ## Run this script from the project root to build the Java SDK:
 # 'bash sdk/java/publish.sh [pomfile] [jarfile] [version] [profile]'
 
-echo "---- Deploying artifacts ('$1' version '$3')... ----";
-set -x
-mvn -Prelease,$4 \
-    -f $1 \
-    gpg:sign-and-deploy-file \
-      -DrepositoryId=github \
-      -Durl=https://maven.pkg.github.com/OpenCannabis/protocol \
-      -DpomFile=$1 \
-      -Dfile=$2 \
-      -Dsdk.revision=$3;
+#echo "---- Deploying artifacts ('$1' version '$3')... ----";
+#set -x
+#mvn -Prelease,$4 \
+#    -f $1 \
+#    gpg:sign-and-deploy-file \
+#      -DrepositoryId=github \
+#      -Durl=https://maven.pkg.github.com/OpenCannabis/protocol \
+#      -DpomFile=$1 \
+#      -Dfile=$2 \
+#      -Dsdk.revision=$3;
